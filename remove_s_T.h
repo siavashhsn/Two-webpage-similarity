72229e88e02570bfbb08f3fcdd95ce33122bb67f
siavashhsn <siavash.hasanpoor@gmail.com>
2016-03-26 15:49:19 +0430

    - remove_s_T.h

--- remove_s_T.h
+++ remove_s_T.h
@@ -1,5 +1,4 @@
 #include "stdafx.hpp"
-#include <map>
 #include <regex>
 #include <math.h>
 #include <string>
@@ -14,22 +13,24 @@ using namespace boost;
 void sim(unordered_map<string, size_t>, unordered_map<string, size_t>);
 
 
-///
-/// \brief listTheWords get stopwordlist.txt
-///         and save in stopw string
-///
-void v_Stopw(unordered_map<string, size_t>& stopw)
-{
-    string s;
-    ifstream ifs ("StopWordList.txt");
-    while(!ifs.eof())
-    {
-        ifs >> s;
-        if(s != "\n" && s != " ")
-            stopw.insert({s,0});
-    }
-    ifs.close();
-}
+///**
+// * @brief v_Stopw   listTheWords get stopwordlist.txt
+// *                  and save in stopw string
+// * @param stopw
+// */
+//void v_Stopw(unordered_map<string, size_t>& stopw)
+//{
+//    string s;
+//    ifstream ifs ("StopWordList.txt");
+//    while(!ifs.eof())
+//    {
+//        ifs >> s;
+//        if(s != "\n" && s != " ")
+//            stopw.insert({s,0});
+//    }
+//    ifs.close();
+//}
+
 
 /**
  * @brief r_stopw function to remove stop words from source codes text
@@ -37,7 +38,15 @@ void v_Stopw(unordered_map<string, size_t>& stopw)
 unordered_map<string, size_t> r_stopw()
 {
     unordered_map<string, size_t> sm;
-    v_Stopw(sm);
+    string s;
+    ifstream ifs ("StopWordList.txt");
+    while(!ifs.eof())
+    {
+        ifs >> s;
+        if(s != "\n" && s != " ")
+            sm.insert({s,0});
+    }
+    ifs.close();
     return sm;
 }
 
@@ -57,7 +66,6 @@ void wToFile(string s, string d)
     w.close();
 }
 
-
 /**
  * @brief get_source_txt get the saved source code
  * @param url name of the saved file
@@ -76,11 +84,6 @@ string get_source_txt(string url)
     return ss;
 }
 
-
-
-
-
-
 /**
  * @brief r_tags remove the html tags and scrit and stylesheet
  * @method using regular expression to replace all these unneccessary
@@ -88,62 +91,46 @@ string get_source_txt(string url)
  */
 void r_tags()
 {
-    string surl1, surl2;
-    vector<string> vurl1, vurl2;
-    unordered_map<string, size_t> murl1, murl2, miurl, maurl, sw;
-    surl1=get_source_txt("url1");
-    surl2=get_source_txt("url2");
+    string urls[2];
 
+    unordered_map<string, size_t> murl1, murl2, sw;
+    urls[0]=get_source_txt("url1");
+    urls[1]=get_source_txt("url2");
 
-    regex parenthesis("[^[:alpha:]]"),       //to remove non-alphanumeric characters
-          rss("\\s+"),                       //to remove \s
-          _script("<script(.*?)</script>"),  //to remove <script> tags and stuff between them
-          stylesheet("<style(.*?)</style>"), //to remove <style> tags and stuff between them
-          html_tags("</?(.|\n)*?>");         //to remove html tags
+    regex re[5];
+    re[0]="<script(.*?)</script>";   //to remove <script> tags and stuff between them
+    re[1]="<style(.*?)</style>";     //to remove <style> tags and stuff between them
+    re[2]="</?(.|\n)*?>";            //to remove html tags
+    re[3]="[^[:alpha:]]";            //to remove non-alphanumeric characters
+    re[4]="\\s+";                    //to remove \s
 
-    surl1 = regex_replace(surl1, _script, " ");
-    surl1 = regex_replace(surl1, stylesheet, " ");
-    surl1 = regex_replace(surl1, html_tags, " ");
-    surl1 = regex_replace(surl1, parenthesis, " ");
-    surl1 = regex_replace(surl1, rss, " ");
-    wToFile("url1s.txt", surl1);
-
-    surl2 = regex_replace(surl2, rss, " ");
-    surl2 = regex_replace(surl2, _script, " ");
-    surl2 = regex_replace(surl2, stylesheet, " ");
-    surl2 = regex_replace(surl2, html_tags, " ");
-    surl2 = regex_replace(surl2, parenthesis, " ");
-    surl2 = regex_replace(surl2, rss, " ");
-    wToFile("url2s.txt", surl2);
-
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<5; j++) {
+            urls[i] = regex_replace(urls[i], re[j], " ");
+        }
+        if(i==0)
+            wToFile("url1s.txt", urls[0]);
+        else
+            wToFile("url2s.txt", urls[1]);
+    }
 
     s = (char *) malloc(i_max+1);
     FILE *u1 = fopen("url1s.txt", "r");
-    murl1 = stemfile(u1);
+    stemfile(u1, murl1);
     FILE *u2 = fopen("url2s.txt", "r");
-    murl2 = stemfile(u2);
+    stemfile(u2, murl2);
     free(s);
 
-//    cout<<"\n[MURL1]"<<endl;
-//    for(auto x:murl2) {
-//        cout<<x.first<< " "<<x.second<<endl;
+    //sw = r_stopw();
+//    for(auto& i:sw)
+//    {
+//        auto ss=murl1.find(i.first);
+//        if(murl1.end() != ss)
+//            murl1.erase(i.first);
+//        ss=murl2.find(i.first);
+//        if(murl2.end() != ss)
+//            murl2.erase(i.first);
 //    }
-//    cout<<"\n\n[MURL2]\n"<<endl;
-
-//    for(auto x:murl2) {
-//        cout<<x.first<< " "<<x.second<<endl;
-//    }
-
-    sw = r_stopw();
-    for(auto& i:sw)
-    {
-        auto ss=murl1.find(i.first);
-        if(murl1.end() != ss)
-            murl1.erase(i.first);
-        ss=murl2.find(i.first);
-        if(murl2.end() != ss)
-            murl2.erase(i.first);
-    }
 
     string ss;
     for(auto& i:murl1)
