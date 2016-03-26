45b25d70942ad51fa1e10bf279e88e834f0f1779
siavashhsn <siavash.hasanpoor@gmail.com>
2016-03-26 16:07:58 +0430

    @ fixed input

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "stdafx.hpp"
-#include "remove_s_T.h"
+#include "removestuff.h"
 #include "save-webpages-source-code.h"
 using namespace std;
 
 int main()
 {
-    string    url1 = "http://www.lipsum.com/"
-            , url2 = "https://en.wikipedia.org/wiki/Regular_expression";
+    string    url1 //= "https://en.wikipedia.org/wiki/Boost_(C%2B%2B_libraries)"
+            , url2; //= "https://en.wikipedia.org/wiki/Regular_expression";
 
-//    sws(url1, "url1.txt");
-//    sws(url2, "url2.txt");
+    cout << "Please insert two urls!" << endl;
+    cout << "url 1: "; cin >> url1;
+    cout << "url 2: "; cin >> url2;
+    sws(url1, "url1.txt");
+    sws(url2, "url2.txt");
     r_tags();
 
     return 0;
